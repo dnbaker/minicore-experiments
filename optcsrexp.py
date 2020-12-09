38dc3220adc9605a0310ff04c7bf69f2f2a2091c
dnbh <dnb@cs.jhu.edu>
2020-12-09 13:25:35 -0500

    Add draft optimization experiment

--- optcsrexp.py
+++ optcsrexp.py
@@ -10,28 +10,20 @@ ps = ap.ArgumentParser("Kmeans++ Experiment")
 
 ps.add_argument("--msr", '-m', type=int, default=5)
 ps.add_argument("--prior", '-P', type=float, default=0.)
-#ps.add_argument('--nthreads', '-p', type=int, default=1)
 ps.add_argument("--dataset", type=str, choices=["pbmc", "cao", "zeisel", "293t", "1.3M", "cao4m"], default="pbmc")
-ps.add_argument("--mbsize", '-B', type=int, default=-1)
 ps.add_argument("--maxiter", '-M', type=int, default=5)
 ps.add_argument("--ncheckins", '-C', type=int, default=1)
 ps.add_argument("--seed", type=int, default=0)
 ps.add_argument("paths", nargs='+')
+ps.add_argument("--mbsize", "-B", action='append')
 
 args = ps.parse_args()
 
+mbsizes = args.mbsize if args.mbsize else [100, 500, 2500, 5000, -1]
+mbsizes = np.array(mbsizes, dtype=np.uint32)
+
 #mc.set_num_threads(args.nthreads)
 
-KSET = [5, 25, 100]
-KMC2 = [0]
-
-NTIMES = 1
-
-matcao = exp_loads[args.dataset]();
-#csrcao = sp.csr_matrix(matcao, dtype=np.float32)
-#csrcao.indices = csrcao.indices.astype(np.uint32)
-#csrcao.indptr = csrcao.indptr.astype(np.uint64)
-smcao = mc.CSparseMatrix(matcao)
 
 def load_ctrs(path):
     toks = path.split(":")
@@ -47,8 +39,32 @@ def load_ctrs(path):
     nkmc = int(nkmc)
     k = int(k)
     assert ds in exp_loads.keys()
-    return {"dataset": exp_loads[ds], 'k': k, 'p': nthreads, 'msr': int(msr), 'prior': prior, 'centers': np.fromfile(path, dtype=np.uint32)}
+    return {"dataset": ds, 'k': k, 'p': nthreads,
+            'msr': int(msr), 'prior': prior, 'centers': np.fromfile(path, dtype=np.uint32)}
+
+def run_opt(mat, ctrs, mbsizes, *, prior, msr, dataset, nthreads):
+    k = ctrs.shape[0]
+    res = []
+    for mbsize in mbsizes:
+        start = gett()
+        out = mc.cluster(mat, ctrs, prior=prior, msr=msr)
+        stop = gett()
+        print(f"{dataset}\t{k}\t{prior}\t{msr}\t{mbsize}\t{nthreads}\t{stop - start}\tout['initcost']\t{out['finalcost']}\t{out['numiter']}\n")
+        out['centers'].astype(np.uint32).tofile("ctrs.{dataset}.{msr}.{prior}.{mbsize}.{nthreads}.result.u32.npy")
+        out['asn'].astype(np.uint32).tofile("asn.{dataset}.{msr}.{prior}.{mbsize}.{nthreads}.result.u32.npy")
+        out['cost'].astype(np.float32).tofile("cost.{dataset}.{msr}.{prior}.{mbsize}.{nthreads}.result.f32.npy")
+    
+print("dataset\tk\tprior\tmsr\tmbsize\tnthreads\truntime\tinitcost\tfinalcost\tnumiter\n")
 
 for path in args.paths:
     inf = load_ctrs(path)
     print(inf)
+    nt = max(inf['p'], 1)
+    mc.set_num_threads(nt)
+    mat = exp_loads[inf['dataset']]()
+    sm = mc.CSparseMatrix(mat)
+    print("Got matrix, getting centers")
+    sel = sm.rowsel(inf['centers'])
+    ctrs = sp.csr_matrix(tuple(sel[:3]), shape=sel[-1]).astype(np.float64).todense()
+    print(ctrs)
+    run_opt(sm, ctrs, mbsizes, prior=args.prior, msr=inf['msr'], dataset=inf['dataset'], nthreads=nt)
