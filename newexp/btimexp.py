d26a9b5303536ef95fdc2f42638734f15a7078c0
dnbh <dnb@cs.jhu.edu>
2021-01-28 11:09:05 -0500

    Add new results, including coreset-based optimization

--- newexp/btimexp.py
+++ newexp/btimexp.py
@@ -1,4 +1,5 @@
 import sys
+from sklearn.datasets import make_classification
 
 import numpy as np
 import scipy.sparse as sp
@@ -14,9 +15,14 @@ from argparse import ArgumentParser as AP
 ap = AP()
 ap.add_argument("--nthreads", "-p", default=32, type=int)
 ap.add_argument("--tol", "-T", default=1e-5, type=float)
+ap.add_argument("--maxiter", default=25, type=int)
 args = ap.parse_args()
 #pbmc = [exp_loads[x]() for x in ['pbmc']][0]
+print("loading data from disk...\n", file=sys.stderr)
 pbmc, cao2, cao4 = [exp_loads[x]() for x in ['pbmc', 'cao', 'cao4m']]
+
+print("loaded data from disk...\n", file=sys.stderr)
+maxiter=args.maxiter
 
 t0 = time()
 pbmc_csr = sp.csr_matrix((pbmc.data, pbmc.indices, pbmc.indptr), shape=pbmc.shape)
@@ -36,11 +42,19 @@ print(f"cao2 load: {time() - t0}", file=sys.stderr)
 mc.set_num_threads(args.nthreads)
 print("Loaded data, processing with %d threads" % mc.get_num_threads(), file=sys.stderr)
 KSET = [3, 10, 25, 50, 100]
-print("#Name\tk\tSKL KM++ time\tSKL KM++ cost\tMC KM++ time\tMC KM++ cost\tMC KM++ plus LS++\tMC KM++ plus LS++ runtime\tSKL KM time\tSKL KM cost\tMC MBKM time\tMC MBKM cost\tMC EMKM time\tMC EMKM cost\tMC CSKM time\tMC CSKM cost")
+print("#Name\tk\tSKL KM++ time\tSKL KM++ cost\tMC KM++ time\tMC KM++ cost\tMC KM++ plus LS++\tMC KM++ plus LS++ runtime\tSKL KM time\tSKL KM cost\tMC MBKM time\tMC MBKM cost\tMC CSKM time\tMC CSKM cost")
+MBSIZE=10000
+
+
+fakedata = make_classification(n_samples=20000, n_classes=10, n_clusters_per_class=2, n_informative=12)[0]
+fakedata[np.abs(fakedata) < 1.] = 0.
+tiny_csr = sp.csr_matrix(fakedata.astype(np.float32))
+tiny_csm = mc.CSparseMatrix(tiny_csr)
 
 def print_set(csr, csm, *, name, kset=KSET):
     for k in kset:
         if k > 50 and csr.shape[0] > 1000000: continue
+        print(f"Performing k={k} for name = {name}", file=sys.stderr)
         t = time()
         try:
             ctrs, ids = skc.kmeans_plusplus(csr, n_clusters=k, n_local_trials=1)
@@ -71,18 +85,21 @@ def print_set(csr, csm, *, name, kset=KSET):
             print("Failed to fit CSR matrix", file=sys.stderr)
         t2 = time()
             
-        
+        mbs = min(int(csr.shape[0]) >> 1, MBSIZE)
         t7 = time()
-        mckmo = mc.hcluster(csm, list(map(int, mco[0])), msr="SQRL2", eps=args.tol, mbsize=min(csr.shape[0], 5000))
+        mckmo = mc.hcluster(csm, list(map(int, mco[0])), msr="SQRL2", eps=args.tol, mbsize=mbs, maxiter=maxiter)
         t8 = time()
         t9 = time()
-        mckmo_cs = mc.hcluster(csm, list(map(int, mco[0])), msr="SQRL2", eps=args.tol, use_cs=True, mbsize=min(csr.shape[0], 5000))
+        mckmo_cs = mc.hcluster(csm, list(map(int, mco[0])), msr="SQRL2", eps=args.tol, use_cs=True, mbsize=mbs, maxiter=maxiter)
+        with open("outfile." + name + ".mckmo.pyd", "wb") as f:
+            from pickle import dump
+            dump(mckmo_cs, f)
         t10 = time()
-        t3 = time()
-        mckmo_em = mc.hcluster(csm, list(map(int, mco[0])), msr="SQRL2", eps=args.tol)
-        t4 = time()
-        print(f"{t2 - t}\t{skkmcost}\t{t8 - t7}\t{mckmo['finalcost']}\t{t4 - t3}\t{mckmo_em['finalcost']}\t{t10 - t9}\t{mckmo_cs['finalcost']}", flush=True)
+        #t3 = time()
+        #mckmo_em = mc.hcluster(csm, list(map(int, mco[0])), msr="SQRL2", eps=args.tol)
+        #t4 = time()
+        print(f"{t2 - t}\t{skkmcost}\t{t8 - t7}\t{mckmo['finalcost']}\t{t10 - t9}\t{mckmo_cs['finalcost']}", flush=True)
 
-print_set(pbmc_csr, pbmc_csm, name="pbmc")
+print_set(tiny_csr, tiny_csm, name="tinyrick")
 print_set(cao4_csr, cao4_csm, name="Cao4m")
 print_set(cao2_csr, cao2_csm, name="Cao2m")
