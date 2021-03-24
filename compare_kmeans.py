283525a7de4acafeee0ec9e9aa653d5b1d2ac64a
Daniel Baker <dnb@cs.jhu.edu>
2021-03-24 15:06:50 -0400

    Delete compare_kmeans.py

--- compare_kmeans.py
+++
@@ -1,8 +0,0 @@
-from load_lazy import exp_loads, ordering
-
-from argparse import ArgumentParser as ap
-
-a = ap()
-a.add_argument("experiment", help="must be 'cao', 'pbmc', '1.3M', '293t', 'zeisel'")
-
-args = a.parse_args()
